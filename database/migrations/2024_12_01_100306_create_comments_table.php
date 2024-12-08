bedc04eeaed4686edb9adb385c34f1ca6cc83ef1
Keaton Anthony Murray <murraykeaton422@gmail.com>
2024-12-07 23:37:38 -0500

    cleaned up migration files

--- database/migrations/2024_12_01_100306_create_comments_table.php
+++ database/migrations/2024_12_01_100306_create_comments_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             ->onUpdate('cascade');
 
             $table->string('comment_count');
+            $table->longText('comment');
 
             $table->timestamps();
         });
